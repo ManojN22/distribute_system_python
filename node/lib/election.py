3f774d00423b4877cb6ec7765d2cb00dde888a3d
ManojN22 <manojkumar22072001@gmail.com>
2024-11-22 14:55:01 -0500

    update

--- node/lib/election.py
+++ node/lib/election.py
@@ -21,13 +21,15 @@ class Worker:
         self.random_timeout = random_timeout
         self.state = State.SLAVE
         self.term = 0
-        self.voted = False
     
     def run_slave(self):
         Thread(target=self.check_heartbeat).start()
     
     def run_candidate(self):
-        Thread(target=self.check_heartbeat).start()
+        Thread(target=self.ask_vote).start()
+
+    def run_leader(self):
+        Thread(target=self.heart_beat).start()
         
     
     def check_heartbeat(self):
@@ -41,15 +43,19 @@ class Worker:
                 Thread(target=self.run_candidate).start()
                 # print("leader failed")
 
-    def receive_heartbeat(self, private_ip, term):
-        if()
+    def receive_heartbeat(self, leader_ip, term):
+        if(term > self.term):
+            self.term = term
+            self.leader_ip = leader_ip    
+            self.state = State.SLAVE  
+            self.run_slave()  
         self.last_hearbeat = time()
 
         return "received the beat"
     
     def heart_beat(self):
-        while(True):
-            sleep(0.1)
+        while(self.state == State.LEADER):
+            sleep(0.4)
             self.ping()
 
     def ping(self):
@@ -58,24 +64,32 @@ class Worker:
                 try:
                     server = RPCClient(host=worker['private_ip'], port=worker['port'])
                     server.connect()
-                    print(server.respond_hello(self.private_ip))
+                    print(server.receive_heartbeat(self.private_ip, self.term))
                     server.disconnect()
                 except Exception as e:
                     print(e)
 
     def ask_vote(self):
         for worker in self.other_servers:
+            votes = 1
             if(self.private_ip != worker['private_ip']):
                 try:
                     server = RPCClient(host=worker['private_ip'], port=worker['port'])
                     server.connect()
-                    print(server.respond_hello(self.private_ip))
+                    if(server.voting(self.term)):
+                        votes+=1
                     server.disconnect()
                 except Exception as e:
                     print(e)
+            if(votes>len(self.other_servers)/2):
+                self.state = State.LEADER
+                self.run_leader()
     
-    def voting(self):
-        if(vote==)
+    def voting(self, term):
+        if(term>self.term):
+            self.term = term
+            return True
+        return False
 
 
 
